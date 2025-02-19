b63fd34039207eb3c1f753ad8cc7197db0dd4f26
Bader-abdallah <badreddineabdallah00@gmail.com>
2025-02-19 22:39:55 +0100

    first commit

---
+++ test.java
@@ -0,0 +1,13 @@
+//voiture
+class test {
+    public static void main(String[] args) {
+        Voiture v = new Voiture();
+        v.afficher();
+    }
+}
+//voiture
+class Voiture {
+    public void afficher() {
+        System.out.println("voiture");
+    }
+}
